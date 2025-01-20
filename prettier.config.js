aaa87583a440d71caa0a9beb12918ff062c1885e
vlad <vlad@ya.ru>
2025-01-20 09:29:09 +0000

    feat(prettier): добавил конфигурацию Prettier и настройки форматирования в VSCode

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type import('prettier').Config */
+const config = {
+  printWidth: 110,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSameLine: false, // Заменяет устаревшее jsxBracketSameLine
+  jsxSingleQuote: false,
+};
+
+export default config;
